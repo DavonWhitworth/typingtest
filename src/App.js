a519500d81b60160c11b4d846c98662640117d04
davon <davonwhitworth@gmail.com>
2022-01-10 21:26:15 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,58 +1,103 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react"
+import Styled from 'styled-components';
+import Preview from './Textpreview.js';
+//import { TextInput } from 'react-native'
 
-const nav = styled.a`
-padding: 0.5rem 0;
+
+const TextBlock = Styled.div`
+background: #262626;
+color: #ACBFA4;
+max-width: 70%;
 `
+//font-size: 1.5em;
+const nav = Styled.nav`
+
+`
+
+
+
+
 
 
 function App() {
 
-  const [userInput, setUserInput] = useState([""])
+  const [userInput, setUserInput] = useState([])
 
-  console.log("users " + userInput[4]);
+  console.log("userinput =  " + userInput + "userInput length = " + userInput.length);
 
   const setString = "This is test text for my typing test";
-  var setText = setString.split("");
+  var dotextsMatch = "";
+  var setText = setString.split();
   const time = 60;
   var correctness = [];
 
 
+
+
+
+  const SettingUserInput = (data) => {
+    //newUserInputdata = data.usertext.split("")    "newUserInputdata= " + newUserInputdata
+    console.log("settingUserIput()= " + data);
+    setUserInput(data);
+    return;
+  }
+
   //input field will setUserInput() then 
-  //loop through arrays to check the accuracy of user's input, 
-  //record accuracy in correctness[], 0 untouched (user = undefined); 1 correct; 2 incorrect;
+  /*loop through arrays to check the accuracy of user's input, 
+  record accuracy in correctness[], 1 correct; 0 incorrect;*/
   const checkUserInput = () => {
-    for (let i = 0; i < userInput.length; i++) {
-      if (userInput[i] === undefined) correctness[i] = 0;
-      if (userInput[i] === setText[i]) correctness[i] = 1;
+    console.log("checking user function ran");
+    if (userInput[userInput.length - 1] === setText[userInput.length - 1]) correctness[userInput.length - 1] = 1; //correct
+    else if (userInput[userInput.length - 1] !== setText[userInput.length - 1]) correctness[userInput.length - 1] = 0; //incorrect
+
+    for (let i = 0; i < correctness.length; i++) {
+      if (correctness[i] === 0) dotextsMatch = false
     }
   }
 
 
 
+
+  //useeffect to triggercheckuesrinput
+  useEffect(() => {
+    checkUserInput();
+  }, [userInput]);
+
+
   return (
     <>
 
-      <test>
+      <TextBlock>
         <nav>
           Reset
           {time}
           30, 60, 90, 120
         </nav>
 
-        <texts>
-          <setTextdiv>
+        <div className="texts">
+          <div className="setText">
+            {dotextsMatch}
+            <br />
             {setText}
-          </setTextdiv>
-          <userTextdiv>
-            <input onChange={setUserInput()}
-            />
-          </userTextdiv>
-        </texts>
-      </test>
+
+          </div>
+          <div className="userText">
+            <br />
+            <form>
+              <input
+                type="text"
+                name="usertext"
+                onChange={data => console.log("input data: " + data)}
+                placeholder="Start typing..." />
+
+            </form>
+          </div>
+        </div>
+      </TextBlock>
 
     </>
   );
 }
 
 export default App;
+// SettingUserInput(data)    from onchange on input
---
+++ src/Textpreview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+
+    return (
+        < div
+            This is test text for my typing test
+        />
+    )
+}
