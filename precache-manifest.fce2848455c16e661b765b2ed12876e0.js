7767a904fc53d61bfca106426d9a8c80c3fe7a18
davon <davonwhitworth@gmail.com>
2022-04-18 23:11:12 -0700

    Updates

---
+++ precache-manifest.fce2848455c16e661b765b2ed12876e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30689c9ba4b2ed6a2eca",
+    "url": "/typingtest/static/css/main.81c650cc.chunk.css"
+  },
+  {
+    "revision": "30689c9ba4b2ed6a2eca",
+    "url": "/typingtest/static/js/main.30689c9b.chunk.js"
+  },
+  {
+    "revision": "b2bd209fd14e3926ffc0",
+    "url": "/typingtest/static/js/1.b2bd209f.chunk.js"
+  },
+  {
+    "revision": "ee13e2624e69998fdd35",
+    "url": "/typingtest/static/js/runtime~main.ee13e262.js"
+  },
+  {
+    "revision": "11bcbbb8abc5d26526a528e0745eab41",
+    "url": "/typingtest/index.html"
+  }
+];
