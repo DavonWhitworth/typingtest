c2dac6bf7ae9ca8770b7fab100f463995a0c7b3e
davon <davonwhitworth@gmail.com>
2022-05-23 13:02:04 -0700

    netflifywip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react"
-import Typingtest from './typingtest';
-import Styled from 'styled-components';
-
+import React from "react";
+import Typingtest from "./typingtest.js";
+import Styled from "styled-components";
 
 const MainContainer = Styled.div`
     padding-top: 100px;
     padding-left: 25%;
-`
+`;
 const Description = Styled.header`
     padding-top: 50px;
     margin: 15px;
@@ -14,17 +13,16 @@ const Description = Styled.header`
     text-align: center;
     transform: translate(0, -50%);
     color: white;
-`
-
+`;
 
 function App() {
-
-
-
   return (
     <div>
-      <Description>Start typing to begin, new wpm per term. Click the "Escape" key for a new term. (Does not effect avgWPM)</Description>
-      <MainContainer>        
+      <Description>
+        Start typing to begin, new wpm per term. Click the "Escape" key for a
+        new term. (Does not effect avgWPM)
+      </Description>
+      <MainContainer>
         <Typingtest />
       </MainContainer>
     </div>
